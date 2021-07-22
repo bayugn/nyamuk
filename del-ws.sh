abfa0b338d5ad806e90677c57b13190dc61fe8bc
PHC Dark  Boy <87409181+LolLloLlLolLlLolL-rgb@users.noreply.github.com>
2021-07-22 19:23:01 +0800

    Update del-ws.sh

--- del-ws.sh
+++ del-ws.sh
@@ -24,7 +24,7 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/v2ray/config.json")
 	echo ""
 	echo " Select the existing client you want to remove"
 	echo " Press CTRL+C to return"
-	echo " ==============================="
+	echo " ×××××××××××××××××××××××××××××××"
 	echo "     No  Expired   User"
 	grep -E "^### " "/etc/v2ray/config.json" | cut -d ' ' -f 2-3 | nl -s ') '
 	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
@@ -43,7 +43,7 @@ systemctl restart v2ray
 systemctl restart v2ray@none
 clear
 echo " V2RAY Account Deleted Successfully"
-echo " =========================="
+echo " ××××××××××××××××××××××××××"
 echo " Client Name : $user"
 echo " Expired On  : $exp"
-echo " =========================="
+echo " ××××××××××××××××××××××××××"

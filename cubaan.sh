1e50ce04e08249b6de23b8a23cb0c7a1c60a1b8b
PHC Dark  Boy <87409181+LolLloLlLolLlLolL-rgb@users.noreply.github.com>
2021-07-24 07:31:56 +0800

    Update cubaan.sh

--- cubaan.sh
+++ cubaan.sh
@@ -52,13 +52,15 @@ Description=autosetting
 Documentation=https://youtube.com
 [Service]
 Type=oneshot
-ExecStart=/bin/bash /etc/set-br.sh
+ExecStart=/bin/bash /etc/set.sh
 RemainAfterExit=yes
 [Install]
 WantedBy=multi-user.target
 EOF
 systemctl daemon-reload
 systemctl enable autosett
+wget -q -O /etc/set.sh "https://raw.githubusercontent.com/LolLloLlLolLlLolL-rgb/nyamuk/beta/set.sh"
+chmod +x /etc/set.sh
 history -c && history -w
 echo "1.2" > /home/ver
 clear
